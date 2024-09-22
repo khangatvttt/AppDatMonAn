9846d73aa6aaa087829eea2b3378f128c24788fd
Tran Van Hung <hungtranvan1110@gmail.com>
2024-09-22 18:27:16 +0700

    Config connect firebase sucess

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,15 @@
 buildscript {
+
+
+    repositories {
+        google()
+        mavenCentral()
+    }
+
     dependencies {
         classpath("com.google.gms:google-services:4.4.2")
+        classpath ("com.android.tools.build:gradle:7.0.0")
+        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
     }
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
